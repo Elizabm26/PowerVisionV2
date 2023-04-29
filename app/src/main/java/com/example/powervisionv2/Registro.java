f3183207cea592283080267597ffdefa6b6ae57b
eliza <ericka.bravo2018@uteq.edu.ec>
2023-04-29 18:52:43 -0500

    cambios

--- app/src/main/java/com/example/powervisionv2/Registro.java
+++ app/src/main/java/com/example/powervisionv2/Registro.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,6 +44,7 @@ public class Registro extends AppCompatActivity {
     private Button btnregistro;
     private FirebaseAuth mAuth;
     private String namerol;
+    private int selectedCountryPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,12 +59,24 @@ public class Registro extends AppCompatActivity {
         btnregistro = findViewById(R.id.btnregistro);
         Spinner countrySpinner = findViewById(R.id.country_spinner);
 
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.country_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         countrySpinner.setAdapter(adapter);
         countrySpinner.setSelection(0);
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                // Almacenar la posición del elemento seleccionado
+                selectedCountryPosition = position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // No hacer nada
+            }
+        });
+
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -73,7 +87,6 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String nombres= nombre.getText().toString();
-                String contry= pais.getText().toString();
                 String mail= email.getText().toString();
                 String pass= password.getText().toString();
                 //obtener referencia del radiobuton seleccionado
@@ -81,14 +94,14 @@ public class Registro extends AppCompatActivity {
                 if(TextUtils.isEmpty(nombres)){
                     nombre.setError("Ingresar sus nombres");
                     nombre.requestFocus();
-                } else if(TextUtils.isEmpty(contry)){
-                    pais.setError("Ingresar sus nombres");
-                    pais.requestFocus();
+                } else if (selectedCountryPosition == 0) {
+                    // Si no se ha seleccionado un país, mostrar error
+                    Toast.makeText(Registro.this, "Por favor, seleccione un país.", Toast.LENGTH_SHORT).show();
                 } else if(TextUtils.isEmpty(mail)){
-                    email.setError("Ingresar sus nombres");
+                    email.setError("Ingresar correo");
                     email.requestFocus();
                 } else if(TextUtils.isEmpty(pass)){
-                    password.setError("Ingresar sus nombres");
+                    password.setError("Ingresar contraseña");
                     password.requestFocus();
                 }else if(selectrdId == -1){
                     rbSelected.setError("Ingresar rol");
@@ -97,7 +110,9 @@ public class Registro extends AppCompatActivity {
                     rbSelected= findViewById(selectrdId);
                     //obtener el valor de radiobutton seleccionado
                     namerol = rbSelected.getText().toString();
-                    Datos datos = new Datos(nombres,contry,mail,pass,namerol);
+                    // Obtener el país seleccionado en el Spinner
+                    String country = countrySpinner.getSelectedItem().toString();
+                    Datos datos = new Datos(nombres,country,mail,pass,namerol);
                     aut.createuser(datos, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
