49ea9c5b72c35b86cfff086233b84dc718ccbda7
eliza <ericka.bravo2018@uteq.edu.ec>
2023-04-30 10:11:04 -0500

    cambios

--- app/src/main/java/com/example/powervisionv2/Fragment_planes.java
+++ app/src/main/java/com/example/powervisionv2/Fragment_planes.java
@@ -1,12 +1,23 @@
 package com.example.powervisionv2;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class Fragment_planes extends Fragment {
@@ -14,7 +25,26 @@ public class Fragment_planes extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionRef = db.collection("planes");
+        collectionRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        // Iterar sobre los documentos y obtener la información que necesites
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            Log.d(TAG, document.getId() + " => " + document.getData());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error getting documents.", e);
+                    }
+                });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planes, container, false);
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
     }
 }
